89a7fa571d9e02f80459fb71b7cd316304849b8a
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-10-08 14:03:27 +0200

    ft_print_rev

---
+++ day00/ex05/ft_print_rev.ml
@@ -0,0 +1,7 @@
+let ft_print_rev : string -> unit = fun x ->
+      for len = String.length x downto 1 do
+        print_char(String.get x (len - 1))
+      done;
+      print_char('\n')
+
+let () =  ft_print_rev ("Hello World !")
