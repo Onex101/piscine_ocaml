241096df8863e6bd6bee907402fd9ef23552c35d
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-10-08 14:02:49 +0200

    ft_power

--- day00/ex02/ft_power.ml
+++ day00/ex02/ft_power.ml
@@ -1,12 +1,14 @@
 let rec ft_power : int -> int -> int = fun x y ->
-                            let i = ref x;
-                            let j = ref y;
                             if y = 0 then
                               1
                             else if x = 0 then
                               0
                             else
-                                while j > 0 do
-                                  i := i * x;
-                                  j := j - 1
-                                done
+                              x * ft_power (x) (y - 1)
+
+let () =  print_int(ft_power 2 3);
+          print_char('\n');
+          print_int(ft_power 0 (-3));
+          print_char('\n');
+          print_int(ft_power 2 0);
+          print_char('\n')
