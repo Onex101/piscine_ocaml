82bfd2797ab0e0bc4230a9add85b190530fd2fe6
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-10-08 14:03:59 +0200

    ft_rot_n

---
+++ day00/ex08/ft_rot_n.ml
@@ -0,0 +1,18 @@
+let rot_one c =
+    if (c < 'a' && c < 'A') || (c > 'Z' && c > 'z') then
+        c
+    else if c = 'z' || c = 'Z' then
+        char_of_int ((int_of_char c) - 25)
+    else
+        char_of_int ((int_of_char c) + 1)
+
+let rec ft_rot_n : int -> string -> string = fun n s ->
+    if n > 0 then
+        begin
+            let s2 = String.map rot_one s in 
+            ft_rot_n (n - 1) s2
+        end
+    else
+        s
+
+let () = print_string(ft_rot_n 4 "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
