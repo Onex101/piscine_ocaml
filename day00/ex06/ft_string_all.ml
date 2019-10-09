f648d5e6a7a7b4be26cd16c67d7421a0468631a8
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-10-09 14:00:29 +0200

    stromg and palindrome update

--- day00/ex06/ft_string_all.ml
+++ day00/ex06/ft_string_all.ml
@@ -1,11 +1,15 @@
 let ft_string_all : (char -> bool) -> string -> bool = fun f x ->
       let len = String.length x in
       let rec test i =
-        if (i == len) then false
-        else if (f (String.get x i) != true) then test (i + 1)
-        else true in
+        if (i = len - 1 && f (String.get x i) = true) then true
+        else if (f (String.get x i) != true) then false
+        else test (i + 1) in
       test (0)
+
 
 let is_digit c = c >= '0' && c <= '9'
 
-let (bool) : bool = ft_string_all is_digit "012356789";
+let  () = if (ft_string_all is_digit "012356789a") then
+                    print_string("true")
+                    else
+                    print_string("false")
--- day00/ex07/ft_is_palindrome.ml
+++ day00/ex07/ft_is_palindrome.ml
@@ -2,8 +2,14 @@ let ft_is_palindrome : string -> bool = fun x ->
   let len = String.length x in
     
   let rec test i j =
-    if (i = j) then true
+    if (i >= (len) || j < 0) then true
+    else if (i = j) then true
     else if (String.get x i != String.get x j) then false
     else test (i + 1) (j - 1) in
 
-  test 0 len
+  test 0 (len-1)
+
+let () = if (ft_is_palindrome "racecar") then
+        print_string("true")
+        else
+        print_string("false")
