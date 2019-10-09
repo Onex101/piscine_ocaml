69e75a379e24ed1067c2cfd39b8af4f426b88cde
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-10-09 07:17:34 +0200

    iter update

--- day01/ex06/iter.ml
+++ day01/ex06/iter.ml
@@ -1,12 +1,16 @@
-let power l = l * l
-
-let iter : (int -> int) -> int -> int -> int = fun f x n ->
-    let rec loop x n = 
-        if n = 0 then
-            x
-        else
-            let t = f(x)
-            loop f(x) (n-1) in
-    loop(x n)
+let rec iter f x n =
+	if n < 0 then
+		(-1)
+	else if n = 0 then
+		x
+	else
+		iter f (f x) (n - 1)
     
-(* let () = print_int(power 2 4); *)
+let () = print_int (iter (fun x -> x * x) 2 4);
+        print_char '\n';
+        print_int (iter (fun x -> x - 2) (-4) 42);
+        print_char '\n';
+        print_int (iter (fun x -> x / 2) 42 1);
+        print_char '\n';
+        print_int (iter (fun x -> x * 2) 2 (-1));
+        print_char '\n';
