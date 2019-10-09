d3d706d10f013adc14aab7a4574f7e8fcc51aab4
Onex101 <xeno@zarobi.co.za>
2019-10-09 20:08:30 +0200

    Crossover

--- day02/ex01/crossover.ml
+++ day02/ex01/crossover.ml
@@ -1,2 +1,8 @@
-let crossover lst1 lst2 = 
-    
+let rec crossover l1 l2 =
+	let rec contains i l = match l with
+		| [] -> false
+		| h::t -> if i = h then true else contains i t
+	in
+	match l1 with
+		| [] -> []
+		| h::t -> if (contains h l2) then h::(crossover t l2) else crossover t l2
