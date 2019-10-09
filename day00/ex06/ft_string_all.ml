4310a5b92c0eb661e02f074460b7c71a4bf09769
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-10-09 13:58:46 +0200

    String All Update

--- day00/ex06/ft_string_all.ml
+++ day00/ex06/ft_string_all.ml
@@ -6,7 +6,6 @@ let ft_string_all : (char -> bool) -> string -> bool = fun f x ->
         else true in
       test (0)
 
-
 let is_digit c = c >= '0' && c <= '9'
 
 let (bool) : bool = ft_string_all is_digit "012356789";
