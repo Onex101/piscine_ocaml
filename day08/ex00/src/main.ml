87b2fbba7743776abb41ddcebb9389fbaaa3cb15
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-10-17 06:52:57 +0200

    update complete

--- day08/ex00/src/main.ml
+++ day08/ex00/src/main.ml
@@ -1,3 +1,14 @@
 let () = 
         let oxy = new Oxygen.oxygen in
+        let c = new Carbon.carbon in
+        let cl = new Chlorine.chlorine in
+        let f = new Fluorine.fluorine in
+        let h = new Hydrogen.hydrogen in
+        let na = new Sodium.sodium in
+        print_endline oxy#to_string;
+        print_endline c#to_string;
+        print_endline cl#to_string;
+        print_endline f#to_string;
+        print_endline h#to_string;
+        print_endline na#to_string;
     ()
