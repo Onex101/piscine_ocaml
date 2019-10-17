737117131a21abce1b545ef460e9e80c574975b3
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-10-17 06:52:06 +0200

    atom update

--- day08/ex00/src/atom.ml
+++ day08/ex00/src/atom.ml
@@ -1,11 +1,11 @@
 class virtual atom =
-    object (self)
+    object (this)
         val virtual _name : string
         val virtual _symbol : string
         val virtual _atomic_number : int 
-        method virtual to_string : string
-        method virtual equals : atom -> bool
-        method virtual get_name : string
-        method virtual get_symbol : string
-        method virtual get_atomic_number : int
+        method to_string = "This atom is " ^ _name ^ " it's symbol is " ^ _symbol ^ " and it has an atomic weight of " ^ (string_of_int _atomic_number)
+        method equals (a:atom) = if ((a#get_name = _name) && (a#get_symbol = _symbol) && (a#get_atomic_number = _atomic_number)) then true else false
+        method get_name = _name
+        method get_symbol = _symbol
+        method get_atomic_number = _atomic_number
     end
