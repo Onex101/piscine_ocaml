b7b1030f26a6e2cb1c49fdf53ad4d628dfa07e4f
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-10-17 06:53:05 +0200

    oxygen update

--- day08/ex00/src/oxygen.ml
+++ day08/ex00/src/oxygen.ml
@@ -4,9 +4,4 @@ class oxygen =
         val _name = "oxygen"
         val _symbol = "O"
         val _atomic_number = 16
-        method to_string = "This atom is " ^ _name ^ " it's symbol is " ^ _symbol ^ " and it has an atomic weight of " ^ (string_of_int _atomic_number)
-        method equals a = if ((a#get_name = _name) && (a#get_symbol = _symbol) && (a#get_atomic_number = _atomic_number)) then true else false
-        method get_name = _name
-        method get_symbol = _symbol
-        method get_atomic_number = _atomic_number
     end
